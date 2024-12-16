642458d4b8a5fbdb4b9e096235c93b9ce311243c
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-12-16 06:28:58 +0530

    changes

--- gst.py
+++ gst.py
@@ -22,14 +22,15 @@ def calculate_icx(df, select_MC):
     filtered = df[df['Host MC'] == select_MC]
 
     ans = filtered.groupby(['Host LC', 'Product'], as_index=False).agg({
-        'SU-APL': 'mean',  
+        # 'SU-APL': 'mean',  
         'APL-APD': 'mean'  
     }).reset_index()
     return ans
 
 def main():
     data = load_data("https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv")
-    st.title("GST - Head of GID Task- Process Time Dashboard")
+    st.title("GST - Head of GID Task")
+    st.title("Process Time Dashboard")
     st.subheader("Prepared by Dinuka Avinash")
 
     st.divider()
@@ -47,6 +48,8 @@ def main():
     select_MC_for_icx = st.selectbox('Select MC', data['Host MC'].unique())
     icx_ans = calculate_icx(data, select_MC_for_icx)
     st.write(icx_ans)
+
+    st.divider()
 
 if __name__ == "__main__":
     main()
