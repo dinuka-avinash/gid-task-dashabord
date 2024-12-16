d8093343a78fae5b9813506898de713521e51bbb
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-12-16 05:32:00 +0530

    add main function

--- gst.py
+++ gst.py
@@ -9,15 +9,38 @@ def load_data(sheet_url):
         st.error(f"Error loading data: {str(e)}")
         return None
 
-data = load_data("https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv")
 
 # to select the MC available in the data file
 # select_MC = st.selectbox('Select MC', data['Home MC'].unique())
 # selec_Product = st.selectbox("Select Product", data['Product'].unique())
 
 def calculate_(df, select_MC):
-    # filtered = df[df['Home MC'] == select_MC]
-    ans = df.groupby('Home MC')['SU-APL'].avg().reset_index()
+    filtered = df[df['Home MC'] == select_MC]
+    ans = filtered.groupby('Home MC')['SU-APL'].avg().reset_index()
     ans_by_product = ans.groupby('Product')
 
     return ans_by_product
+
+def main():
+    data = load_data("https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv")
+    
+    st.title("GST")
+    st.write(data)
+    select_MC = st.selectbox('Select MC', data['Home MC'].unique())
+    ans = calculate_(data, select_MC)
+    st.write(ans)
+
+    # col1, col2, col3 = st.columns([1, 1, 1])
+
+    #         # Display the total applications in the first column
+    # with col1:
+    #     st.markdown(
+    #         "<div style='text-align: center;'>"
+    #         f"<h3>🌍 {data_mode} Applications</h3>"
+    #         f"<p style='font-size: 32px;'>{total_applied}</p>"
+    #         "</div>",
+    #         unsafe_allow_html=True,
+    #     )
+
+if __name__ == "__main__":
+    main()
