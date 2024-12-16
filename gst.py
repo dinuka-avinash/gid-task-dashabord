73001dd4b50e8f0b3a721f8df3501b272d9a1658
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-12-16 06:04:15 +0530

    icx and ogx separate tables

--- gst.py
+++ gst.py
@@ -14,7 +14,7 @@ def load_data(sheet_url):
 # select_MC = st.selectbox('Select MC', data['Home MC'].unique())
 # selec_Product = st.selectbox("Select Product", data['Product'].unique())
 
-def calculate_(df, select_MC):
+def calculate_ogx(df, select_MC):
     filtered = df[df['Home MC'] == select_MC]
     # ans = filtered.groupby('Home LC')['SU-APL'].sum().reset_index()
     # ans_by_product = ans.groupby('Product')
@@ -24,25 +24,35 @@ def calculate_(df, select_MC):
     }).reset_index()
     return ans
 
+def calculate_icx(df, select_MC):
+    filtered = df[df['Host MC'] == select_MC]
+    # ans = filtered.groupby('Home LC')['SU-APL'].sum().reset_index()
+    # ans_by_product = ans.groupby('Product')
+    ans = filtered.groupby(['Host LC', 'Product'], as_index=False).agg({
+        'SU-APL': 'mean',  # Example aggregation for SU-APL
+        'APL-APD': 'mean'  # Replace with the actual column name and aggregation method
+    }).reset_index()
+    return ans
+
 def main():
     data = load_data("https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv")
     
     st.title("GST Task - Process Time Dashboard")
-    select_MC = st.selectbox('Select MC', data['Home MC'].unique())
-    ans = calculate_(data, select_MC)
-    st.write(ans)
+    st.divider()
 
-    # col1, col2, col3 = st.columns([1, 1, 1])
+    st.subheader("For oGX Process Times")
 
-    #         # Display the total applications in the first column
-    # with col1:
-    #     st.markdown(
-    #         "<div style='text-align: center;'>"
-    #         f"<h3>🌍 {data_mode} Applications</h3>"
-    #         f"<p style='font-size: 32px;'>{total_applied}</p>"
-    #         "</div>",
-    #         unsafe_allow_html=True,
-    #     )
+    select_MC_for_ogx = st.selectbox('Select MC', data['Home MC'].unique())
+    ogx_ans = calculate_ogx(data, select_MC_for_ogx)
+    st.write(ogx_ans)
+
+    st.divider()
+
+    st.subheader("For oGX Process Times")
+
+    select_MC_for_icx = st.selectbox('Select MC', data['Host MC'].unique())
+    icx_ans = calculate_icx(data, select_MC_for_icx)
+    st.write(icx_ans)
 
 if __name__ == "__main__":
     main()
