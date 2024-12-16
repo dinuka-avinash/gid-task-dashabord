72a5a952bbe120b5b10aec5030f29c957aeeae34
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-12-16 06:07:14 +0530

    title and some text changes

--- gst.py
+++ gst.py
@@ -9,35 +9,30 @@ def load_data(sheet_url):
         st.error(f"Error loading data: {str(e)}")
         return None
 
-
-# to select the MC available in the data file
-# select_MC = st.selectbox('Select MC', data['Home MC'].unique())
-# selec_Product = st.selectbox("Select Product", data['Product'].unique())
-
 def calculate_ogx(df, select_MC):
     filtered = df[df['Home MC'] == select_MC]
-    # ans = filtered.groupby('Home LC')['SU-APL'].sum().reset_index()
-    # ans_by_product = ans.groupby('Product')
+
     ans = filtered.groupby(['Home LC', 'Product'], as_index=False).agg({
-        'SU-APL': 'mean',  # Example aggregation for SU-APL
-        'APL-APD': 'mean'  # Replace with the actual column name and aggregation method
+        'SU-APL': 'mean', 
+        'APL-APD': 'mean'  
     }).reset_index()
     return ans
 
 def calculate_icx(df, select_MC):
     filtered = df[df['Host MC'] == select_MC]
-    # ans = filtered.groupby('Home LC')['SU-APL'].sum().reset_index()
-    # ans_by_product = ans.groupby('Product')
+
     ans = filtered.groupby(['Host LC', 'Product'], as_index=False).agg({
-        'SU-APL': 'mean',  # Example aggregation for SU-APL
-        'APL-APD': 'mean'  # Replace with the actual column name and aggregation method
+        'SU-APL': 'mean',  
+        'APL-APD': 'mean'  
     }).reset_index()
     return ans
 
 def main():
     data = load_data("https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv")
     
-    st.title("GST Task - Process Time Dashboard")
+    st.title("GST - Head of GID Task- Process Time Dashboard")
+    st.subheader("Prepared by Dinuka Avinash")
+
     st.divider()
 
     st.subheader("For oGX Process Times")
@@ -48,7 +43,7 @@ def main():
 
     st.divider()
 
-    st.subheader("For oGX Process Times")
+    st.subheader("For iCX Process Times")
 
     select_MC_for_icx = st.selectbox('Select MC', data['Host MC'].unique())
     icx_ans = calculate_icx(data, select_MC_for_icx)
